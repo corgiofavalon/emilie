354c387563fe595cbbadc92a1d1ad53a166e5379
corgiofavalon <nuggetlala@gmail.com>
2023-05-08 19:47:20 +0800

    e

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'obispo' );
 define( 'DB_PASSWORD', 'tubbynbibbles' );
 
 /** Database hostname */
-define( 'DB_HOST', 'https://corgiofavalon.github.io/emilie/' );
+define( 'DB_HOST', 'corgiofavalon.github.io' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
